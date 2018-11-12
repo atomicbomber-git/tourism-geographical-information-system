eb2d21ba746f2ccb33f32aa5a9b3d165a35d03a5
James Patrick Keegan <atomicbomber.email@gmail.com>
2018-11-12 10:09:46 +0700

    Finished analysis feature.

---
+++ resources/views/site-analysis/create.blade.php
@@ -0,0 +1,71 @@
+@extends('shared.layout')
+@section('title', 'Penentuan Situs Wisata Terbaik dengan Metode AHP')
+@section('content')
+<div class="container my-5">
+    <h1 class='mb-5'>
+        <i class='fa fa-cog'></i>
+        Penentuan Situs Wisata Terbaik dengan Metode AHP
+    </h1>
+
+    <div class="card">
+        <div class="card-header">
+            <i class="fa fa-cog"></i>
+            Penentuan Situs Wisata Terbaik dengan Metode AHP
+        </div>
+        <div class="card-body">
+           <form action="{{ route('site-analysis.result') }}" method="POST">
+               @csrf
+
+                <div class='form-group'>
+                    <label for='visitor_count'> Prioritas Aspek J. Pengunjung: </label>
+                
+                    <input
+                        id='visitor_count' name='visitor_count' type='number'
+                        value='{{ old('visitor_count') }}'
+                        placeholder='Prioritas Aspek J. Pengunjung:'
+                        class='form-control {{ !$errors->has('visitor_count') ?: 'is-invalid' }}'>
+                
+                    <div class='invalid-feedback'>
+                        {{ $errors->first('visitor_count') }}
+                    </div>
+                </div>
+
+                <div class='form-group'>
+                    <label for='fee'> Prioritas Aspek Harga Tiket Masuk: </label>
+                
+                    <input
+                        id='fee' name='fee' type='number'
+                        placeholder="Prioritas Aspek Harga Tiket Masuk"
+                        value='{{ old('fee') }}'
+                        class='form-control {{ !$errors->has('fee') ?: 'is-invalid' }}'>
+                
+                    <div class='invalid-feedback'>
+                        {{ $errors->first('fee') }}
+                    </div>
+                </div>
+
+                <div class='form-group'>
+                    <label for='facility_count'> Prioritas Aspek Jumlah Fasilitas: </label>
+                
+                    <input
+                        id='facility_count' name='facility_count' type='number'
+                        placeholder='Prioritas Aspek Jumlah Fasilitas'
+                        value='{{ old('facility_count') }}'
+                        class='form-control {{ !$errors->has('facility_count') ?: 'is-invalid' }}'>
+                
+                    <div class='invalid-feedback'>
+                        {{ $errors->first('facility_count') }}
+                    </div>
+                </div>
+
+                <div class="form-group text-right">
+                    <button type="submit" class="btn btn-primary">
+                        Lakukan
+                        <i class="fa fa-check"></i>
+                    </button>
+                </div>
+           </form>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/site-analysis/result.blade.php
@@ -0,0 +1,219 @@
+@extends('shared.layout')
+@section('title', 'Situs Wisata Terbaik')
+@section('content')
+<div class="container mt-5">
+    <h1 class="mb-5">
+        <i class="fa fa-list"></i>
+        Situs Wisata Terbaik
+    </h1>
+
+    @auth
+
+    <div class="card">
+        <div class="card-header">
+            <i class="fa fa-icon"></i>
+            Hasil Kalkulasi Prioritas Aspek
+        </div>
+        <div class="card-body">
+           <table class="tabel table-sm table-bordered">
+               <tbody>
+                    <tr class="table-info">
+                        <th> ASPEK </th>
+                        @foreach ($aspect_comparisons as $aspect_key => $comparisons)
+                        <th> {{ $aspects[$aspect_key] }} </th>
+                        @endforeach
+                    </tr>
+                    @foreach ($aspect_comparisons as $aspect_key_r => $comparisons)
+                    <tr>
+                        <th class="table-info"> {{ $aspects[$aspect_key_r] }} </th>
+                        @foreach ($comparisons as $comparison)
+                        <td> @number_format($comparison) </td>
+                        @endforeach
+                    </tr>
+                    @endforeach
+                    <tr class="table-danger">
+                        <th> Total: </th>
+                        @foreach ($aspect_comparisons as $aspect_key => $comparisons)
+                        <td> @number_format($aspect_comparison_sums[$aspect_key]) </td>
+                        @endforeach
+                    </tr>
+               </tbody>
+           </table>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header">
+            <i class="fa fa-icon"></i>
+            Hasil Kalkulasi Prioritas Aspek
+        </div>
+        <div class="card-body">
+           <table class="tabel table-sm table-bordered">
+               <tbody>
+                    <tr class="table-info">
+                        <th> ASPEK </th>
+                        @foreach ($normalized_aspect_comparisons as $aspect_key => $comparisons)
+                        <th> {{ $aspects[$aspect_key] }} </th>
+                        @endforeach
+                        <th> Prioritas </th>
+                    </tr>
+                    @foreach ($normalized_aspect_comparisons as $aspect_key_r => $priorities)
+                    <tr>
+                        <th class="table-info"> {{ $aspects[$aspect_key_r] }} </th>
+                        @foreach ($priorities as $priority)
+                        <td> @number_format($priority) </td>
+                        @endforeach
+
+                        <td> @number_format($aspect_priorities[$aspect_key_r]) </td>
+
+                    </tr>
+                    @endforeach
+
+               </tbody>
+           </table>
+        </div>
+    </div>
+
+    @foreach ($aspects as $aspect_key => $aspect_name)
+    <div class="card mb-5">
+        <div class="card-header">
+            <i class="fa fa-list"></i>
+            Hasil Kalkulasi Prioritas Lokal <strong> {{ $aspect_name  }} </strong>
+        </div>
+        <div class="card-body" style="overflow-y: auto">
+
+            <h3> 1. Tabel Perbandingan Lokal </h3>
+
+            <table class="table table-sm table-bordered">
+                <tbody>
+                    {{-- HEADER ROW --}}
+                    <tr class="table-info">
+                        <th> {{ $aspect_name }} </th>
+
+                        @foreach($local_comparisons[$aspect_key] as $point_id => $comparisons)
+                        <th> {{ $points[$point_id]->name }} </th>
+                        @endforeach
+
+                    </tr>
+
+                    @foreach ($local_comparisons[$aspect_key] as $point_id => $comparisons)
+                    <tr>
+                        <th class="table-info"> {{ $points[$point_id]->name }} </th>
+                        
+                        @foreach ($comparisons as $comparison_point_id => $comparison)
+                        <th> @number_format($comparison) </th>
+                        @endforeach
+
+                    </tr>
+                    @endforeach
+
+                    <tr class="table-danger">
+                        <th> Total </th>
+                        @foreach ($local_comparison_sums[$aspect_key] as $point_id => $sum)
+                        <th> @number_format($sum) </th>
+                        @endforeach
+                    </tr>
+
+                </tbody>
+            </table>
+
+            <h3 class="mt-5"> 2. Tabel Prioritas Lokal </h3>
+
+            <table class="table table-sm table-bordered">
+                <tbody>
+                    {{-- HEADER ROW --}}
+                    <tr>
+                        <th class="table-success"> {{ $aspect_name }} </th>
+
+                        @foreach($local_priorities[$aspect_key] as $point_id => $comparisons)
+                        <th class="table-success"> {{ $points[$point_id]->name }} </th>
+                        @endforeach
+                        
+                        <th class="table-success"> Prioritas </th>
+                    </tr>
+
+                    @foreach ($local_priorities[$aspect_key] as $point_id => $comparisons)
+                    <tr>
+                        <th class="table-info"> {{ $points[$point_id]->name }} </th>
+                        
+                        @foreach ($comparisons as $comparison_point_id => $comparison)
+                        <th> @number_format($comparison) </th>
+                        @endforeach
+                        
+                        <th class="table-warning"> @number_format($comparisons->average()) </th>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+    @endforeach
+
+    <div class="card">
+        <div class="card-header">
+            <i class="fa fa-list"></i>
+            Daftar Prioritas Lokal Per-Aspek
+        </div>
+        <div class="card-body">
+            <table class="table table-sm table-bordered">
+                <thead class="thead thead-dark">
+                    <tr>
+                        <th> Alternatif S. Wisata </th>
+                        @foreach ($aspects as $aspect_name)
+                        <th> {{ $aspect_name }} </th>
+                        @endforeach
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr class="table-info">
+                        <td> --- </td>
+                        @foreach ($aspects as $aspect_key => $aspect_name)
+                        <td> @number_format($aspect_priorities[$aspect_key]) </td>
+                        @endforeach
+                    </tr>
+
+                    @foreach ($points as $point)
+                    <tr>
+                        <td> {{ $point->name }} </td>
+                        @foreach ($aspects as $aspect_key => $aspect_name)
+                        <td> @number_format($local_priorities[$aspect_key][$point->id]->average()) </td>
+                        @endforeach
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    @endauth
+
+    <div class="card mt-3">
+        <div class="card-header">
+            Situs Terbaik
+        </div>
+        <div class="card-body">
+            <div class='table-responsive'>
+                <table class='table table-sm table-bordered'>
+                   <thead class="thead thead-dark">
+                        <tr>
+                            <th> # </th>
+                            <th> Nama </th>
+                            <th> Nilai </th>
+                        </tr>
+                   </thead>
+                   <tbody>
+                       @foreach ($overall_priorities as $point_id => $priority)
+                        <tr>
+                            <td> {{ $loop->iteration }}. </td>
+                            <td> {{ $points[$point_id]->name }} </td>
+                            <td> @number_format($priority["average"]) </td>
+                        </tr>
+                       @endforeach
+                   </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
