40aa6e2408556dcd2c51b02cd1fe2b50e86adf25
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-09-01 16:20:42 +0700

    Updated.

--- resources/js/components/SiteDetailModal.vue
+++ resources/js/components/SiteDetailModal.vue
@@ -1,6 +1,6 @@
 <template>
   <modal :name="name" height="auto">
-        <div class="p-4" style="position: relative" v-if="selected_point">
+        <div class="p-4" style="height: 420px; overflow-y: scroll; position: relative" v-if="selected_point">
             <div class="text-right" style="position: absolute; top: 10px; right: 10px;">
                 <button class="btn btn-default" @click="onClose">
                     <i class="fa fa-times"></i>
@@ -11,16 +11,28 @@
                 {{ selected_point.name }}
             </h1>
 
-            <dl>
-                <dt> Jumlah Pengunjung </dt>
-                <dd> {{ number_format(selected_point.site.visitor_count) }} </dd>
+            <div class="row mb-3">
+                <div class="col-sm-12 col-md-6">
+                    <img
+                        width="200px"
+                        :src="`/site/thumbnail/${selected_point.site.id}`"
+                        class="img-fluid"
+                        :alt="selected_point.site.name">
+                </div>
 
-                <dt> Harga Tiket Masuk </dt>
-                <dd> {{ number_format(selected_point.site.fee) }} </dd>
+                <div class="col-sm-12 col-md-6">
+                    <dl>
+                        <dt> Jumlah Pengunjung </dt>
+                        <dd> {{ number_format(selected_point.site.visitor_count) }} </dd>
 
-                <dt> Jumlah Fasilitas </dt>
-                <dd> {{ number_format(selected_point.site.facility_count) }} </dd>
-            </dl>
+                        <dt> Harga Tiket Masuk </dt>
+                        <dd> {{ number_format(selected_point.site.fee) }} </dd>
+
+                        <dt> Jumlah Fasilitas </dt>
+                        <dd> {{ number_format(selected_point.site.facility_count) }} </dd>
+                    </dl>
+                </div>
+            </div>
 
             <p>
                 {{ selected_point.site.description }}
